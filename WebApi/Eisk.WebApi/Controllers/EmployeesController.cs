a2b998352125d3f6a0fc041d4830ecc71a14a7ec
AshrafAlam <joy_csharp@yahoo.com>
2019-02-19 23:06:52 +0800

    moved routing to base controller.

--- WebApi/Eisk.WebApi/Controllers/EmployeesController.cs
+++ WebApi/Eisk.WebApi/Controllers/EmployeesController.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Eisk.WebApi.Controllers
+﻿namespace Eisk.WebApi.Controllers
 {
     using Eisk.Core.WebApi;
     using Domains.Entities;
     using DomainServices;
 
-    [Route("api/[controller]")]
     public class EmployeesController : WebApiControllerBaseAsync<Employee,int>
     {
         public EmployeesController(EmployeeDomainService employeeDomainService):base(employeeDomainService)
