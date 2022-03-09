11d823020a8bcd18482696119698d1cdbd09558e
Ashraf Alam <joy_csharp@yahoo.com>
2022-03-09 12:57:20 +0000

    added employee controller with get method

---
+++ WebApi/Eisk.WebApi2/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿namespace Eisk.WebApi.Controllers
+{
+    using Eisk.Core.WebApi;
+    using Domains.Entities;
+    using DomainServices;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
+    using System;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeeDomainService _employeeDomainService;
+        public EmployeesController(EmployeeDomainService employeeDomainService)
+        {
+            _employeeDomainService = employeeDomainService;
+        }
+
+        [HttpGet(Name = "Employees")]
+        public IEnumerable<Employee> Get()
+        {
+            return _employeeDomainService.GetAll().Result;
+        }
+    }
+}
--- WebApi/Eisk.WebApi2/Controllers/WeatherForecastController.cs
+++ WebApi/Eisk.WebApi2/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace Eisk.WebApi2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
