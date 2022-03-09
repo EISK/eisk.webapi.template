6e24b26a1ae4192d5d10b3e6e58eab144cafd49f
Ashraf Alam <joy_csharp@yahoo.com>
2022-03-09 13:41:43 +0000

    refactored eisk web project to exclude unnecessary files.

--- WebApi/Eisk.WebApi/Controllers/EmployeesController.cs
+++ WebApi/Eisk.WebApi/Controllers/EmployeesController.cs
@@ -4,62 +4,14 @@
     using Domains.Entities;
     using DomainServices;
     using Microsoft.AspNetCore.Mvc;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using System.Linq;
-    using System;
-
-    //[ApiController]
-    //[Route("[controller]")]
-    //public class EmployeesController : ControllerBase
-    //{
-    //    EmployeeDomainService _employeeDomainService;
-    //    public EmployeesController(EmployeeDomainService employeeDomainService)
-    //    {
-    //        _employeeDomainService = employeeDomainService;
-    //    }
-
-    //    [HttpGet(Name = "Employees")]
-    //    public IEnumerable<Employee> Get()
-    //    {
-    //        return _employeeDomainService.GetAll().Result;
-    //    }
-    //}
 
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class EmployeesController : WebApiControllerBase<Employee, int>
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"};
-
-        public WeatherForecastController()
+        public EmployeesController(EmployeeDomainService employeeDomainService):base(employeeDomainService)
         {
             
         }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
-
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
     }
 }
--- WebApi/Eisk.WebApi/Program.cs
+++ WebApi/Eisk.WebApi/Program.cs
@@ -1,61 +1,37 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
+using Eisk.DataServices.EFCore;
+using Eisk.DataServices.EFCore.DataContext;
+using Eisk.DataServices.Interfaces;
+using Eisk.DomainServices;
+using Eisk.EFCore.Setup;
 
-namespace Eisk.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//custom services
+
+builder.Services.AddScoped<AppDbContext, InMemoryDbContext>();
+builder.Services.AddScoped<IEmployeeDataService, EmployeeDataService>();
+builder.Services.AddScoped<EmployeeDomainService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Eisk.WebApi",
-                    Version = "v1.0-preview-1",
-                    Description = "EISK makes it easy to write scalable and secured web api on top of Microsoft's new cutting edge .net core technologies.",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "EISK Web Api",
-                        Email = string.Empty,
-                        Url = new Uri("https://eisk.github.io/eisk.webapi")
-                    }
-                });
-            });
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
