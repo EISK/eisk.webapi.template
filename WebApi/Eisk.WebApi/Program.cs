d2c089523c88cfbab5b65594559fb0f5657524b6
Ashraf Alam <ashraf.alam@schroders.com>
2022-03-16 15:31:05 +0000

    -- updated swagger reference

--- WebApi/Eisk.WebApi/Program.cs
+++ WebApi/Eisk.WebApi/Program.cs
@@ -42,7 +42,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v2");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
