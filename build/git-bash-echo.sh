2501d72536280c7e5a0ca0577a744e11cfbf2041
Ashraf Alam <ashraf.alam@schroders.com>
2022-04-12 15:35:51 +0100

    updated bash script

--- build/git-bash-echo.sh
+++ build/git-bash-echo.sh
@@ -1,3 +1,5 @@
+echo "############################# Passed Params (before) 1 - $1, 2 - $2, 3 - $3, 4 - $4, 5 - $5"
+
 ContentTargetGitAddress=${1:-https://github.com/EISK/eisk.webapi.git}
 echo "############################# ContentTargetGitAddress ContentTargetGitAddress"
 
@@ -23,3 +25,5 @@ echo "############################# SOURCE_DIR $SOURCE_DIR"
 
 TEMP_REPO_DIR=$PWD/$ContentTargetGitRepoDownloadFolder
 echo "############################# TEMP_REPO_DIR $TEMP_REPO_DIR"
+
+echo "############################# Passed Params (after) 1 - $1, 2 - $2, 3 - $3, 4 - $4, 5 - $5"
