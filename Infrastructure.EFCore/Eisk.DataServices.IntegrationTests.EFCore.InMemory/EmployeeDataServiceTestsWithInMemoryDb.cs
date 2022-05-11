4c7c1fa65be9cd090b250165f29e0a873970614c
Ashraf Alam <ashraf.alam@schroders.com>
2022-05-11 18:22:53 +0100

    v9.0.23, using namespaces move to top

--- Infrastructure.EFCore/Eisk.DataServices.IntegrationTests.EFCore.InMemory/EmployeeDataServiceTestsWithInMemoryDb.cs
+++ Infrastructure.EFCore/Eisk.DataServices.IntegrationTests.EFCore.InMemory/EmployeeDataServiceTestsWithInMemoryDb.cs
@@ -1,9 +1,9 @@
-namespace Eisk.DataServices.IntegrationTests.EFCore.InMemory;
-
-using Domains.Entities;
 using Eisk.DataServices.EFCore;
+using Eisk.Domains.Entities;
 using Eisk.EFCore.Setup;
-using Test.Core.TestBases;
+using Eisk.Test.Core.TestBases;
+
+namespace Eisk.DataServices.IntegrationTests.EFCore.InMemory;
 
 public class EmployeeDataServiceTestsWithInMemoryDb : DataServiceBaseIntegrationTests<Employee, int>
 {
